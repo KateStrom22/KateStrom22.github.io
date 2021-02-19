585129c708fff15f2bde756f8392ebad3d82b928
kstrom20 <kstrom20@byui.edu>
2021-02-18 20:36:44 -0600

    week 6 set up

---
+++ week6/preston/js/scripts.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+document.getElementById('last').textContent = document.lastModified;
+
+var d = new Date();
+if (d.getDay() == 5) {
+    alert("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.");
+}
