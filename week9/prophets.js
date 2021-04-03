cad04e7cf402fa1c26f8620d12028ceceac03add
kstrom20 <kstrom20@byui.edu>
2021-04-02 20:28:34 -0500

    cards refuse to work

--- week9/prophets.js
+++ week9/prophets.js
@@ -14,11 +14,20 @@ fetch(requestURL)
 
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
+            let birth = document.createElement('p');
+            let place = document.createElement('p');
+            let image = document.createElement('img');
             
             h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            birth.textContent = "Birth Date: " + prophets[i].birthdate;
+            place.textContent = "Birth Place : " + prophets[i].birthplace;
+            image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', (prophets[i].name +" " +prophets[i].lastname + "-" + prophets[i].order));
 
             card.appendChild(h2);
-
+            card.appendChild(birth);
+            card.appendChild(place);
+            card.appendChild(image);
             document.querySelector('div.cards').appendChild(card);
 
         // temporary checking for valid response and data parsing
