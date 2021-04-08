37ebcd3790ed1b3e4a8909c9bb8c0ef1a4f0cc72
kstrom20 <kstrom20@byui.edu>
2021-04-07 19:23:31 -0500

    started 5 day forcast w10

---
+++ week10/weatherapi.js
@@ -0,0 +1,16 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=ebb6dc23c3a5e521b7977dbc0df05103&units=imperial';
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        const temperature = document.querySelector('#temperature');
+        temperature.textContent = jsObject.main.temp;
+
+        const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;  // note the concatenation
+        const desc = jsObject.weather[0].description;  // note how we reference the weather array
+        document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+        document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
+
+    });
